9dfb2eb4d23c59101b25fec6345a185efdd5c557
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-06-23 22:09:04 -0400

    updated the team UI

--- public/views/team.vue
+++ public/views/team.vue
@@ -1,35 +1,21 @@
 <template>
-    <div class="d-flex flex-items-start">
-        <div class="d-flex flex-justify-center">
+    <div class="Box Box--condensed border-0">
+        <div class="Box-header border-0 d-flex flex-items-center">
             <img class="CircleBadge avatar avatar-small mr-3" alt="jonrohan" v-bind:src="getAdmin.photoURL" width="32" height="32" />
-            <div>
-                <span>{{getAdmin.displayName}}</span>
+            <div v-on:click="googleSignOut()">
+                <span class="h4 d-block">{{getAdmin.displayName}}</span>
                 <span class="f6 d-block">{{getAdmin.email}}</span>
             </div>
+            <span class="State ml-3">+{{online}}</span> 
         </div>
-        <div class="px-6">
-            <div class="AvatarStack AvatarStack--five-plus ">
-                <div class="AvatarStack-body tooltipped tooltipped-se tooltipped-align-left-1">
-                    <img v-for="user in userlist" class="CircleBadge avatar avatar-lg" v-bind:src="user.src" width="32" height="32" />
-                </div>
-            </div>
-        </div>
+    </div>
 </template>
 <script>
-module.exports = {
-    components: {
-        'user': httpVueLoader('../views/user.vue')
-    },
+module.exports = { 
     data: function() {
         return {
             title: "teams",
-            userlist: [{
-                src: 'https://github.com/jonrohan.png?v=3&s=96'
-            }, {
-                src: 'https://github.com/jonrohan.png?v=3&s=96'
-            }, {
-                src: 'https://github.com/jonrohan.png?v=3&s=96'
-            }]
+            online:"14" 
         }
     },
     methods: {
@@ -46,16 +32,7 @@ module.exports = {
 }
 </script>
 <style scoped>
-ul {
-    padding: 0;
-    margin: 0;
-    list-style: none;
-}
-
-li {
-    margin: 10px;
-    border-radius: 5px;
-    display: grid;
-    grid-template-columns: 1fr;
+.Box{
+    cursor: pointer;
 }
 </style>
